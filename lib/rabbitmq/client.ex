dc892e00c0ee7980af403bcf4b39700a789749a3
apellizz <alberto.pellizzon@hotmail.it>
2019-09-04 18:16:17 +0200

    Wip

--- lib/rabbitmq/client.ex
+++ lib/rabbitmq/client.ex
@@ -1,7 +1,21 @@
 defmodule RabbitClient do
   require Logger
+
   defstruct [:brokerUrl, :connection, :channel, :timeout, consumers: []]
 
+  defmodule Receiver do
+    def wait_for_messages(client) do
+      receive do
+        {:basic_deliver, payload, meta} ->
+          Delivery.fromAmqpDelivery(meta, payload)
+          wait_for_messages(client)
+        {:EXIT, _from, _reason} ->
+
+      end
+    end
+  end
+
+  @spec connect(RabbitClient.t()) :: RabbitClient.t() | :error
   def connect(client) do
     Logger.info("Connecting to broker...")
 
@@ -11,11 +25,17 @@ defmodule RabbitClient do
       Logger.info("Connection successful")
       %RabbitClient{client | connection: conn, channel: chan}
     else
-      {:error, err} -> Logger.error(err)
-      _ -> Logger.error("Urecognized error")
+      {:error, err} ->
+        Logger.error(err)
+        :error
+
+      _ ->
+        Logger.error("Unrecognized error")
+        :error
     end
   end
 
+  @spec stop(RabbitClient.t()) :: :ok | :error
   def stop(client) do
     Logger.info("Stopping RabbitMQ client...")
 
@@ -23,8 +43,75 @@ defmodule RabbitClient do
          :ok <-
            AMQP.Connection.close(client.connection) do
       Logger.info("RabbitMQ client stopped")
+      :ok
     else
-      {:error, _} -> Logger.error("Connection teardown failed")
+      {:error, _} ->
+        Logger.error("Connection teardown failed")
+        :error
+    end
+  end
+
+  @spec cast(RabbitClient.t(), Context.t(), Delivery.t()) :: nil
+  def cast(client, ctx, request) do
+    doPublish(
+      client,
+      request.exchange,
+      request.routing_key,
+      ctx.correlationId,
+      request.headers,
+      request.body,
+      ctx.replyTo
+    )
+  end
+
+  def listen(client, ctx, queue, auto_ack, exclusive, no_local, no_wait, args, handler) do
+    with {:ok, consumer_tag} <-
+           AMQP.Basic.consume(client.channel, queue,
+             no_local: no_local,
+             no_wait: no_wait,
+             arguments: args,
+             exclusive: exclusive,
+             no_ack: !auto_ack
+           ) do
+              %RabbitClient{client | consumers: [client.consumers | consumer_tag]}
+              |> Receiver.wait_for_messages()
+           else
+            error -> Logger.error(error)
+           end
+    end
+  end
+
+  @spec doPublish(
+          RabbitClient.t(),
+          String.t(),
+          String.t(),
+          String.t(),
+          any,
+          String.t(),
+          String.t() | nil
+        ) :: nil
+  defp doPublish(client, exchange, routing_key, correlationId, headers, body, reply_to) do
+    try do
+      AMQP.Basic.publish(client.channel, exchange, routing_key, body,
+        headers: headers,
+        persistent: true,
+        reply_to: if(reply_to, do: reply_to, else: :undefined),
+        timestamp: Time.utc_now(),
+        content_type: "application/vnd.google.protobuf"
+      )
+
+      nil
+    catch
+      err ->
+        Logger.error(%{
+          error: err,
+          exchange: exchange,
+          key: routing_key,
+          body: body,
+          correlationId: correlationId
+        })
+
+        nil
     end
   end
 end
---
+++ lib/rabbitmq/delivery.ex
@@ -0,0 +1,21 @@
+defmodule Delivery do
+  defstruct [:headers, :body, :exchange, :routing_key]
+
+  def fromResponse(exchange, routing_key, response) do
+    %Delivery{
+      exchange: exchange,
+      routing_key: routing_key,
+      body: response.payload,
+      headers: response.headers
+    }
+  end
+
+  def fromAmqpDelivery(meta, payload) do
+    %Delivery{
+      headers: meta.headers,
+      body: payload,
+      exchange: meta.exchange,
+      routing_key: meta.routing_key
+    }
+  end
+end
