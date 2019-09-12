f7d07dadc48be32f5ddf6c581f395e91fb241b4e
apellizz <alberto.pellizzon@hotmail.it>
2019-09-12 14:34:35 +0200

    Add rebar to pipeline

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 use Mix.Config
 
+IO.puts("mix test started with folliwing configuration")
+IO.inspect(System.get_env())
 config(:coniglio, :broker_url, System.get_env("AMQP_URL") || "amqp://localhost:5672")
 config(:coniglio, :client, Coniglio.RabbitClient.RealClient)
